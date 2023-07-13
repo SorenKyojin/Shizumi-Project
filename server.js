c3545891d729801512cdaeb9d42882a4eb276692
SorenKyojin <otakus.ichiro@gmail.com>
2023-07-13 14:58:29 +0200

    Shizumi Server

--- server.js
+++ server.js
@@ -104,3 +104,22 @@ function selectChar() {
             });
     });
 }
+
+function getPlayerWallet() {
+  getPlayerUUID();
+  fetch('database/players/' + userId +'.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(playerdata) {
+    console.log(playerdata.playerwallet);
+    OkaneWallet = playerdata.playerwallet;
+  })
+  .catch(function(error) {
+    console.log('Une erreur s\'est produite : ' + error.message);
+  });
+}
+
+if (OkaneWallet == null) {
+  OkaneWallet = 0;
+}
