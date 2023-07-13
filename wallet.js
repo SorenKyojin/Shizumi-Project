e069e19dbb3531155f1655b04a3faeb20c8a06e4
SorenKyojin <otakus.ichiro@gmail.com>
2023-07-13 15:00:05 +0200

    Player wallet management

--- wallet.js
+++ wallet.js
@@ -1,20 +1,2 @@
-function getPlayerWallet() {
-    getPlayerUUID();
-    fetch('database/players/' + userId +'.json')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(playerdata) {
-      console.log(playerdata.playerwallet);
-    })
-    .catch(function(error) {
-      console.log('Une erreur s\'est produite : ' + error.message);
-    });
-}
-OkaneWallet = playerdata.playerwallet;
-
-if (OkaneWallet == null) {
-    OkaneWallet = 0;
-}
-// 
+getPlayerWallet();
 document.getElementById("okane-wallet-value").innerHTML = OkaneWallet;
