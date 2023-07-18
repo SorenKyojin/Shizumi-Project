930969a4575a412155e91883d6a4ebda868954d1
SorenKyojin <otakus.ichiro@gmail.com>
2023-07-18 14:57:23 +0200

    Home page

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Shizumi est un jeu sur navigateur inspiré de Mudae, visant à permettre aux joueurs de collectionner leurs personnages préférés.">
     <link href='https://fonts.googleapis.com/css?family=PT Sans' rel='stylesheet'>
     <!-- ! Dans le link ci-dessus, le validateur W3C détecte une erreur lié à l'espace.
     ! Cependant, il n'est pas possible de l'enlever ou le remplacer, sinon la police ne fonctionne plus -->
