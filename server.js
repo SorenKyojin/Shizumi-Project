d7c155f2275941771c67eef2d203660a84413e6c
SorenKyojin <otakus.ichiro@gmail.com>
2023-07-21 14:07:21 +0200

    Shizumi Server

--- server.js
+++ server.js
@@ -14,25 +14,6 @@ app.get('/shutdown', (req, res) => {
       process.exit(0);
     });
 });
-
-// Cette route va permettre d'envoyer la donnée que l'on cherche dans le JSON du joueur stocké dans le server vers son client pour retourner la valeur
-// C'est comme ça que l'on peut afficher le montant d'un porte-monnaie virtuel, comme le porte-monnaie Steam par exemple.
-app.get('/getPlayerWallet', function(req, res) {
-  // ? Ici, on veut récupérer la valeur du porte-monnaie du joueur. D'abord, il faut trouver son UUID.
-  fetch('/database/players/' + userId + '.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    OkaneWallet = data.playerwallet;
-  })
-  .catch(function(error) {
-    console.log('Une erreur s\'est produite :', error);
-  });
-  getPlayerWallet();
-  // Une fois que getPlayerWallet() a terminé son exécution et que la variable OkaneWallet a été mise à jour, vous pouvez envoyer la réponse contenant la valeur d'OkaneWallet
-  res.json({ OkaneWallet: OkaneWallet });
-});
 app.post('/getPlayerEmail', (req, res) => {
   // Accédez à la valeur de l'email dans le corps de la requête
   const email = req.body.email;
@@ -43,6 +24,42 @@ app.post('/getPlayerEmail', (req, res) => {
 
   // Répondez à la requête PHP avec une réponse appropriée
   res.send('Email reçu avec succès !');
+});
+function getUserId() {
+  connection.query('SELECT id FROM users WHERE email = ?', email, (err, results) => {
+  if (err) {
+    console.error('Erreur lors de l\'exécution de la requête :', err);
+    return;
+  }
+  
+  if (results.length > 0) {
+    const userId = results[0].id;
+    console.log('Identifiant de l\'utilisateur actuel :', userId);
+  }
+});
+function getPlayerData() {
+  fetch("database/players/" + userId + ".json")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        // Ici, "data" contient les données JSON du fichier sélectionné aléatoirement
+        // Pour l'instant pour des fins de test, on affiche les données dans la console.
+        console.log(data);
+      })
+      .catch(function(error) {
+          console.log('Une erreur s\'est produite : ' + error.message);
+      });
+}
+}
+// ? On va chercher dans le JSON correspondant au joueur, la valeur de playerwallet pour l'afficher sur le client
+app.get('/getPlayerWallet', function(req, res) {
+  getUserId();
+  // ? Ici, on veut récupérer la valeur du porte-monnaie du joueur. D'abord, il faut trouver son UUID.
+  getPlayerData();
+  getPlayerWallet();
+  // Une fois que getPlayerWallet() a terminé son exécution et que la variable OkaneWallet a été mise à jour, vous pouvez envoyer la réponse contenant la valeur d'OkaneWallet
+  res.json({ OkaneWallet: OkaneWallet });
 });
 
 // * Connexion à la base de données
@@ -55,62 +72,46 @@ const connection = mysql.createConnection({
     database: 'shizumi'
 });
 connection.connect((err) => {
+  // ? On vérifie si une erreur survient
     if (err) {
       console.error('Erreur de connexion à la base de données :', err);
       return;
     }
     console.log('Connexion à la base de données établie !');
 });
-
-function getUserId() {
-  connection.query('SELECT id FROM users WHERE email = ?', email, (err, results) => {
-  if (err) {
-    console.error('Erreur lors de l\'exécution de la requête :', err);
-    return;
-  }
-  
-  if (results.length > 0) {
-    const userId = results[0].id;
-    console.log('Identifiant de l\'utilisateur actuel :', userId);
-    // Tu peux stocker userId dans une variable ou l'utiliser comme nécessaire.
-  }
-});
-}
-function getPlayerData() {
+function parsePlayerData() {
+  // ? On obtient les données du joueur via la méthode fs et JSON.parse()
   const playerData = fs.readFileSync('database/players/' + userId + '.json');
   const player = JSON.parse(playerData);
 }
 function getPlayerRolls() {
-  // Pour obtenir la valeur de "rolls" du joueur
+  // ? Pour obtenir la valeur de "rolls" du joueur
   const rolls = player.rolls;
 }
 function selectChar() {
+  // * Sélection d'un personnage aléatoire de la BDD de personnages
   fs.readdir(baseCharacterPath, (err, charFiles) => {
     if (err) {
       console.error('Erreur lors de la lecture du dossier :', err);
       return;
     }
-  
     totalCharFiles = charFiles.length;
     console.log('Nombre de fichiers dans le dossier :', totalCharFiles);
-  
-       // Ici, vous pouvez continuer avec le code pour générer un nombre aléatoire et accéder au fichier correspondant.
-       // Par exemple, vous pouvez utiliser la méthode précédente pour générer un nombre aléatoire entre 1 et le nombre de fichiers,
-       // puis construire le chemin vers le fichier JSON sélectionné aléatoirement et le lire.
-       
-       // Étape 2 : Générez un nombre aléatoire entre 1 et le nombre total de fichiers
+      
+      // Étape 2 : On génère un nombre aléatoire pour sélectionner un personnage dans la BDD
+      // ! Cette méthode est la méthode de base. Dans la version finale de Shizumi, le calcul sera beaucoup plus complexe, et devra prendre en compte
+      // ! les bonus du joueur
     randomCharFile = Math.floor(Math.random() * totalCharFiles) + 1;
     
-       // Étape 3 : Utilisez le numéro de fichier aléatoire pour construire le chemin vers le fichier JSON correspondant
+      // Étape 3 : On utilise le résultat du calcul pour en faire le fichier à sélectionné.
     selectedCharFile = randomCharFile + '.json';
-       // Étape 4 : Effectuez une requête HTTP pour récupérer le fichier JSON sélectionné
+      // Étape 4 : On effectue une requête HTTP pour récupérer le fichier JSON sélectionné
     fetch(selectedCharFile)
       .then(function(response) {
         return response.json();
       })
       .then(function(data) {
-        // Ici, "data" contient les données JSON du fichier sélectionné aléatoirement
-        // Faites ce que vous voulez avec les données, par exemple les afficher dans la console
+        // ? "data" contient les données JSON du fichier sélectionné aléatoirement
         console.log(data);
         characterNameText.textContent = data.name;
         characterSerieText.textContent = data.serie;
