80619777fa05aa0f2992b0ebc8ffd02550530194
SorenKyojin <otakus.ichiro@gmail.com>
2023-07-25 15:17:22 +0200

    Error page

---
+++ error.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Shizumi est un jeu sur navigateur inspiré de Mudae, visant à permettre aux joueurs de collectionner leurs personnages préférés.">
+    <link href='https://fonts.googleapis.com/css?family=PT Sans' rel='stylesheet'>
+    <!-- ! Dans le link ci-dessus, le validateur W3C détecte une erreur lié à l'espace.
+    ! Cependant, il n'est pas possible de l'enlever ou le remplacer, sinon la police ne fonctionne plus -->
+    <link rel="stylesheet" href="style.css">
+    <script src="popup.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <title>Shizumi - Erreur</title>
+</head>
+<body>
+    
+</body>
+</html>
