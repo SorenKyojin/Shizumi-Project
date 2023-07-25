55f5868e7fb9741bcedeac8ed87178cefd692c94
SorenKyojin <otakus.ichiro@gmail.com>
2023-07-25 09:58:09 +0200

    Forgotten password form

---
+++ forgot.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href='https://fonts.googleapis.com/css?family=PT Sans' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+    <script src="wallet.js"></script>
+    <script src="popup.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <title>Shizumi - Mot de passe oublié</title>
+</head>
+<body>
+    <main>
+        <div class="box center-all">
+            <h2>Mot de passe oublié</h2>
+            <p>Pour vérifier votre identité vous recevrez un email qui vous permettra de changer le mot de passe.</p>
+            <form action="" method="post">
+                <label for="email-forgot">Email</label>
+                <input type="email" name="email-forgot" id="email-forgot">
+                <input type="submit" value="Envoyer">
+            </form>
+        </div>
+    </main>
+</body>
+</html>
