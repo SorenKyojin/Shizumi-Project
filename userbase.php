0ef6fed99bcb25107849cf9590ac301a0ff52a9e
SorenKyojin <otakus.ichiro@gmail.com>
2023-07-26 16:02:40 +0200

    Admin user profiles list

---
+++ userbase.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href='https://fonts.googleapis.com/css?family=PT Sans' rel='stylesheet'>
+    <!-- ! Dans le link ci-dessus, le validateur W3C détecte une erreur lié à l'espace.
+    ! Cependant, il n'est pas possible de l'enlever ou le remplacer, sinon la police ne fonctionne plus -->
+    <link rel="stylesheet" href="style.css">
+    <script src="popup.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <title>Base de joueurs - Shizumi</title>
+</head>
+<body>
+    <div>
+        <h1>Joueurs sur Shizumi</h1>
+    </div>
+    
+</body>
+</html>
