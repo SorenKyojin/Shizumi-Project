66712726053d90902c6713d49efca347898e0f38
Soren <otakus.ichiro@gmail.com>
2023-07-27 09:36:55 +0200

    User profile settings

--- settings.php
+++ settings.php
@@ -45,16 +45,16 @@ session_start();
                     }
                     $email = $_SESSION['email'];
                     try {
-                        // Prépare la requête SQL en utilisant un paramètre nommé :email
+                        // Prepare SQL request with :email parameter
                         $sql = "SELECT id FROM users WHERE email = :email";
 
-                        // Prépare la requête avec PDO
+                        // Prepare the request with PDO protocol
                         $query = $conn->prepare($sql);
 
-                        // Exécute la requête en liant la valeur de l'e-mail
+                        // Execute the request, with the value of email
                         $query->execute(['email' => $email]);
 
-                        // Récupère le résultat de la requête
+                        // Get the result of the request
                         $result = $query->fetch();
                         if ($result) {
                             $userID = $result['id'];
@@ -66,9 +66,9 @@ session_start();
                         echo "Erreur : " . $err->getMessage();
                     }
                     ?></p>
-                <!-- On affiche l'UID unique de l'utilisateur. Celui-ci n'est pas modifiable, et sert à stocker la collection de l'utilisateur (et essentiellement ses informations) -->
+                <!-- We show player's UUID. It can't be modified, and is used to store everything the player owns in a JSON file (and some informations, like the wallet) -->
             </div>
-            <!-- Icône de la page -->
+            <!-- Profile picture -->
             <img src="img/default_profile_pics/<?php
                     try {
                         $cnn = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_pass);
@@ -78,16 +78,16 @@ session_start();
                     }
                     $email = $_SESSION['email'];
                     try {
-                        // Prépare la requête SQL en utilisant un paramètre nommé :email
+                        // Prepare SQL request with :email parameter
                         $sql = "SELECT default_profile_picture FROM users WHERE email = :email";
 
-                        // Prépare la requête avec PDO
+                        // Prepare the request with PDO protocol
                         $query = $conn->prepare($sql);
 
-                        // Exécute la requête en liant la valeur de l'e-mail
+                        // Execute the request, with the value of email
                         $query->execute(['email' => $email]);
 
-                        // Récupère le résultat de la requête
+                        // Get the result of the request
                         $result = $query->fetch();
                         if ($result) {
                             $default_pfp = $result['default_profile_picture'];
@@ -114,16 +114,16 @@ session_start();
                     }
                     $email = $_SESSION['email'];
                     try {
-                        // Prépare la requête SQL en utilisant un paramètre nommé :email
+                        // Prepare SQL request with :email parameter
                         $sql = "SELECT username FROM users WHERE email = :email";
 
-                        // Prépare la requête avec PDO
+                        // Prepare the request with PDO protocol
                         $query = $conn->prepare($sql);
 
-                        // Exécute la requête en liant la valeur de l'e-mail
+                        // Execute the request, with the value of email
                         $query->execute(['email' => $email]);
 
-                        // Récupère le résultat de la requête
+                        // Get the result of the request
                         $result = $query->fetch();
                         if ($result) {
                             $username = $result['username'];
@@ -149,22 +149,22 @@ session_start();
                         }
                         $email = $_SESSION['email'];
                         try {
-                            // Prépare la requête SQL en utilisant un paramètre nommé :email
+                            // Prepare SQL request with :email parameter
                             $sql = "SELECT firstname FROM users WHERE email = :email";
 
-                            // Prépare la requête avec PDO
+                            // Prepare the request with PDO protocol
                             $query = $conn->prepare($sql);
 
-                            // Exécute la requête en liant la valeur de l'e-mail
+                            // Execute the request, with the value of email
                             $query->execute(['email' => $email]);
 
-                            // Récupère le résultat de la requête
+                            // Get the result of the request
                             $result = $query->fetch();
                             if ($result) {
                                 $username = $result['firstname'];
                                 echo $username;
                             } else {
-                                echo "Indiquez votre prénom";
+                                echo "Aucun";
                             }
                         } catch (PDOException $err) {
                             echo "Erreur : " . $err->getMessage();
@@ -227,16 +227,16 @@ session_start();
                     }
                     $email = $_SESSION['email'];
                     try {
-                        // Prépare la requête SQL en utilisant un paramètre nommé :email
+                        // Prepare SQL request with :email parameter
                         $sql = "SELECT default_profile_picture FROM users WHERE email = :email";
 
-                        // Prépare la requête avec PDO
+                        // Prepare the request with PDO protocol
                         $query = $conn->prepare($sql);
 
-                        // Exécute la requête en liant la valeur de l'e-mail
+                        // Execute the request, with the value of email
                         $query->execute(['email' => $email]);
 
-                        // Récupère le résultat de la requête
+                        // Get the result of the request
                         $result = $query->fetch();
                         if ($result) {
                             $default_pfp = $result['default_profile_picture'];
