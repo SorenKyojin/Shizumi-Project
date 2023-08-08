2a2ea4d1b7a12932888ba8fd481b8170ec31ce43
SorenKyojin <otakus.ichiro@gmail.com>
2023-08-08 13:57:30 +0200

    Added icon support for random tips

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
-var tips = [
-    "Pour pouvoir jouer directement à Shizumi sur mobile, ajoutez le site sur votre écran d’accueil.",
-    "Cliquez sur le logo de Shizumi ou votre photo de profil pour revenir au menu",
-    "Planter une orchidée offre 5 Okanes par niveau lorsque vous obtenez un personnage.",
+tips = [
+    "Pour pouvoir jouer directement à Shizumi sur mobile, ajoutez le site sur votre écran d’accueil.", // 1
+    "Cliquez sur le logo de Shizumi ou votre photo de profil pour revenir au menu", // 2
+    "Planter une orchidée offre 5 Okanes par niveau lorsque vous obtenez un personnage.", // 3...
     "Pensez à arroser vos plantes afin de ne pas les laisser mourrir et ainsi garder vos bonus.",
     "Obtenez des graines en faisant apparaître des personnages liés aux plantes, ou en achetant un dans la boutique.",
     "Un engrais coûte 100 Okanes. Les plantes communes requièrent 1 engrais par niveau, 2 pour les plantes rares et 5 pour les très rares.",
@@ -11,3 +11,32 @@ var tips = [
     "Si un personnage que vous possédez déjà appraraît dans votre loterie, vous pouvez récupérer le double de sa valeur.",
     "Lorsqu'un personnage que vous possédez déjà apparaît dans votre loterie, sa valeur en Okane augmentera après avoir réclamé."
 ];
+
+// Les icônes doivent correspondent au texte !
+icon = [
+    "info.png", // 1
+    "info.png", // 2
+    "plant.png", // 3...
+    "plant.png",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+];
+
+
+// Fonction pour obtenir un indice aléatoire
+function indiceAleatoire(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// Obtenir un indice aléatoire pour sélectionner un texte
+indice = indiceAleatoire(texts.length);
+
+
+
+document.getElementById('tip-text').textContent = tips[indice];
+document.getElementById('tip-icon').src = "img/tips-icon/" + icon[indice];
