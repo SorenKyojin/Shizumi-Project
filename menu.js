2a5cf1b0332d8a92ead328a18a4fe4bd7a5430c9
SorenKyojin <otakus.ichiro@gmail.com>
2023-08-08 11:59:20 +0200

    Adding tips

--- menu.js
+++ menu.js
@@ -1,13 +1,13 @@
-window.onload = function(){
-    var menuVisible = 0;
-}
-function menuToggle() {
-    if (menuVisible == 1) {
-        menuVisible = 0;
-        document.getElementById(menu).style.visibility = hidden;
-    }
-    else{
-        menuVisible = 1;
-        document.getElementById(menu).style.visibility = visible;
-    }
-}
+var tips = [
+    "Pour pouvoir jouer directement à Shizumi sur mobile, ajoutez le site sur votre écran d’accueil.",
+    "Cliquez sur le logo de Shizumi ou votre photo de profil pour revenir au menu",
+    "Planter une orchidée offre 5 Okanes par niveau lorsque vous obtenez un personnage.",
+    "Pensez à arroser vos plantes afin de ne pas les laisser mourrir et ainsi garder vos bonus.",
+    "Obtenez des graines en faisant apparaître des personnages liés aux plantes, ou en achetant un dans la boutique.",
+    "Un engrais coûte 100 Okanes. Les plantes communes requièrent 1 engrais par niveau, 2 pour les plantes rares et 5 pour les très rares.",
+    "Les plantes légendaires sont très difficile à trouver. Vous pouvez en planter qu'une seule dan votre jardin, et n'est améliorable.",
+    "Le buisson donne des fruits pouvant être donnés aux personnages que vous possédez ou que vous souhaitez.",
+    "Donner une fraise à un personnage que vous possédez augmentera sa valeur en Okane.",
+    "Si un personnage que vous possédez déjà appraraît dans votre loterie, vous pouvez récupérer le double de sa valeur.",
+    "Lorsqu'un personnage que vous possédez déjà apparaît dans votre loterie, sa valeur en Okane augmentera après avoir réclamé."
+];
