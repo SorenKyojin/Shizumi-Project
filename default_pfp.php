377de1aba7c439979a2f9bf794a57c9145c5effa
SorenKyojin <otakus.ichiro@gmail.com>
2023-08-11 14:53:13 +0200

    Added 2 new default pfp

--- default_pfp.php
+++ default_pfp.php
@@ -1,6 +1,6 @@
 <?php
 $photoFolder = 'img/default-profile-pics/';
-$photos = ["crystal.png","cyberpunk_girl.png","lady_alyx.png","pink.png","sacha_inspired.png","silver_blue.png","vibrance.jpg","welt.png"];
+$photos = ["crystal.png","cyberpunk_girl.png","lady_alyx.png","pink.png","sacha_inspired.png","silver_blue.png","vibrance.jpg","welt.png","blue.jpg","butterfly.jpg"];
 shuffle($photos);
 $randomPhoto = $photos[0];
 ?>
